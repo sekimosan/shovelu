88e8249738718f27a2b59837fe8035a593e885aa
sekimosan <persephone.0930@gmail.com>
2021-03-29 19:19:16 +0900

    フォローボタンの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def edit
   end
   def update
   end  
+  def show
+    @user = User.find(params[:id])
+  end  
+
 end
