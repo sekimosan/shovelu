d5508e88f96a628a0d9fbc2819171c11edcf5d8c
sekimosan <persephone.0930@gmail.com>
2021-04-05 11:27:23 +0900

    RSpec,faker,FactoryBotの導入

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,16 @@
 class Room < ApplicationRecord
-  validates :name, presence: true
-  
   has_many :user_rooms
   has_many :users, through: :user_rooms
   has_many :topics, dependent: :destroy
   
+  validates :name, presence: true
+  validate :topics_up_to_6
 
+  private
+  TOPIC_MAX = 6
+  def topics_up_to_6
+    if self.topics.size > TOPIC_MAX
+      errors.add(:topics,"topics are up to 6")
+    end  
+  end  
 end
