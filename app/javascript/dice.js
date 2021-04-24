a9a3b62a3e9524fe3afb3e3fe9bd751179c46006
sekimosan <persephone.0930@gmail.com>
2021-04-24 17:57:06 +0900

    Update dice.js

--- app/javascript/dice.js
+++ app/javascript/dice.js
@@ -6,7 +6,8 @@ function dice (){
 			const randomNum = Math.floor(Math.random() * 6) + 1;
 			const shakeDice = randomNum + ".png";
 			const diceImage = document.getElementById("saikoro");
-			diceImage.innerHTML = "<img src='/assets/" + shakeDice + "' alt='sample'>";
+			//diceImage.innerHTML = "<img src='/assets/"+ shakeDice +"' alt='sample'>";
+			diceImage.innerHTML = "<img src='https://hotate1104.s3-ap-northeast-1.amazonaws.com/"+ shakeDice +"' alt='sample'>";
       count++;
 		};
 		var setI = setInterval(() =>{shake();if(count > 20){clearInterval(setI);}},100);	
