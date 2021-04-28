dcefa7a47052d86e40a82c085b9f2d21c0a8cf8d
sekimosan <persephone.0930@gmail.com>
2021-04-28 15:02:29 +0900

    バリデーション修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,10 +6,9 @@ class Topic < ApplicationRecord
   validate :room_topics_up_to_6
 
   private
-  TOPIC_MAX = 6
   def room_topics_up_to_6
-    if self.room && self.room.topics.count > TOPIC_MAX
-      errors.add(:topic,"topics are up to 6")
+    if self.room && self.room.topics.count >= 6
+      errors.add(:topic,"トピックは６つまでです")
     end
   end    
 
