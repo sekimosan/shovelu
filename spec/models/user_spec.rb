a31035884c034c982139a934f9331c2fbc9fa602
sekimosan <persephone.0930@gmail.com>
2021-04-29 18:41:13 +0900

    ユーザー仮データ作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe "ユーザー新規登録" do
+    context "新規登録が成功するとき" do
+      it "nickname,email,user_name,password,password_confirmation,age_id,gender_id,profileが正しく記入されている" do
+        expect(@user).to be_valid
+      end
+      it "profileがなくても登録ができる" do
+        @user.profile = nil
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
