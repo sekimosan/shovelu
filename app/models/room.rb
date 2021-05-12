cec6b3a9e9840dec89345cbbe98e448403e9da4a
sekimosan <persephone.0930@gmail.com>
2021-05-12 21:38:06 +0900

    ルーム保存応急処置

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
-  has_many :users, through: :user_rooms
+  has_many :users, through: :user_rooms, validate: false
   has_many :topics, dependent: :destroy
   
   
